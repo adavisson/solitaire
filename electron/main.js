2a085587f4a5ae685e98ca258ed92adbf04704d2
Andrew Davisson <akdavisson4@gmail.com>
2020-07-20 10:17:11 -0600

    initial electron config

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const electron = require('electron')
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow //Global window object
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({height: 600, width: 800, show: false})
+
+  const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+
+  mainWindow.loadURL(startURL)
+
+  mainWindow.once('ready-to-show', () => mainWindows.show())
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+//mac things
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
