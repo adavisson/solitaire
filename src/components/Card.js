eea864b30d13fde41b4ea66c2cd79b7b7a53134c
Andrew Davisson <akdavisson4@gmail.com>
2020-07-21 08:33:40 -0600

    added props and coloring

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Card = () => {
+const Card = (props) => {
   const [facedown, setFacedown] = useState(false)
+  const [suit, setSuit] = useState('')
+
+  useEffect(() => {
+    switch(props.suit) {
+      case 'clubs':
+        setSuit('\u2663')
+        break
+      case 'diamonds':
+        setSuit('\u2666')
+        break
+      case 'hearts':
+        setSuit(`\u2665`)
+        break
+      case 'spades':
+        setSuit('\u2660')
+    }
+  },[])
 
   const flip = () => {
     setFacedown(!facedown)
@@ -10,15 +27,15 @@ const Card = () => {
   return (
     <div className='card' onClick={flip}>
       {!facedown && (
-        <div className='card-front'>
+        <div className={props.suit === 'diamonds' || props.suit === 'hearts' ? 'card-front red' : 'card-front'}>
           <div className='top-index'>
-            3<br />
-            &spades;
+            {props.value}<br />
+            {suit}
           </div>
-          <div className='card-center'>&spades;</div>
+          <div className='card-center'>{suit}</div>
           <div className='bottom-index'>
-            3<br />
-            &spades;
+            {props.value}<br />
+            {suit}
           </div>
         </div>
       )}
