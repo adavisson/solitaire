06b3c77042c892db2f154408780a8a9e8cf87475
Andrew Davisson <akdavisson4@gmail.com>
2020-07-21 08:12:46 -0600

    removed images and use html characters

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import spade from '../assets/spade.png'
 
 const Card = () => {
   const [facedown, setFacedown] = useState(false)
@@ -12,7 +11,7 @@ const Card = () => {
     <div className='card' onClick={flip}>
       {!facedown && (
         <div className='card-front'>
-          <img className='card-center' src={spade} alt="spade" />
+          <div className="card-center">&spades;</div>
         </div>
       )}
     </div>
