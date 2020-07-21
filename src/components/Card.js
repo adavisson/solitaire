4a0919c69e153c1d4e7f603fc4cd509e3cf0fce7
Andrew Davisson <akdavisson4@gmail.com>
2020-07-20 22:15:20 -0600

    adding card front and card center

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react'
+import spade from '../assets/spade.png'
 
 const Card = () => {
+  const [facedown, setFacedown] = useState(false)
+
   return (
-    <div className="card">
-      
+    <div className='card'>
+      {!facedown && (
+        <div className='card-front'>
+          <img className='card-center' src={spade} alt="spade" />
+        </div>
+      )}
     </div>
-  );
+  )
 }
- 
-export default Card;
+
+export default Card
