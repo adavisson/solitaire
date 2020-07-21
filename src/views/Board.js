67137c83d290a33e8374ec31665a2b803cc9eb08
Andrew Davisson <akdavisson4@gmail.com>
2020-07-21 14:22:59 -0600

    formatting

--- src/views/Board.js
+++ src/views/Board.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 import Card from '../components/Card'
 
 const Board = () => {
   return (
-    <div className="board">
-      <Card value="3" suit="spades" />
+    <div className='board'>
+      <Card className='drag' value='3' suit='spades' />
     </div>
-  );
+  )
 }
- 
-export default Board;
+
+export default Board
