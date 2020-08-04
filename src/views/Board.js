9d7943e92b165c402fcca0ebf969457146bf1c18
Andrew Davisson <akdavisson4@gmail.com>
2020-08-04 15:10:37 -0600

    added Space style

--- src/views/Board.js
+++ src/views/Board.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/Card'
+import Space from '../components/Space'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
@@ -7,7 +8,13 @@ const Board = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className='board'>
-        <Card className='drag' value='3' suit='spades' />
+        <Space
+          id='draw-stack'
+          style={{ marginLeft: '1.5em', marginTop: '1.5em' }}
+        >
+          {/* <Card className='drag' value='3' suit='spades' /> */}
+        </Space>
+        
       </div>
     </DndProvider>
   )
